c625361fe8535f77cff295fed1dd629d88a7c187
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-08-26 14:43:35 +0300

    build matrix

--- mathematics-and-python/week-2/task-1.py
+++ mathematics-and-python/week-2/task-1.py
@@ -1,6 +1,8 @@
 import os
 import re
 import itertools
+import numpy as np
+from collections import Counter
 
 def read_file(name):
   dir_path = os.path.dirname(os.path.realpath(__file__))
@@ -16,14 +18,21 @@ class Line():
   def __init__(self, line):
     self.line = line
     self.gist = self.__to_gist(line)
+    self.counter = Counter(self.gist)
+
+  def weight(self, term):
+    return self.counter[term]
 
   def __to_gist(self, line):
     arr = re.split('[^a-z]', line)
     return list(filter(None, arr))
 
-lines = map(lambda line: Line(line), read_file("sentences.txt"))
+lines = list(map(lambda line: Line(line), read_file("sentences.txt")))
 terms = uniq_terms(map(lambda line: line.gist, lines))
-print(terms)
+weights = map(lambda line:
+  list(map(lambda term: print(line.weight(term)), terms))
+  ,lines)
+print(np.array(list(weights)))
 # строки токенизировать в массив
 # массив почистить
 # все уникальные слова - gist
