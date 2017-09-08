5e3416c9ae8e2970192c17574e325581e8f9e678
Sverchkov Nikolay <ssnikolay@gmail.com>
2017-09-09 00:13:31 +0300

    Pretty code

--- mathematics-and-python/week-4/pareto.py
+++ mathematics-and-python/week-4/pareto.py
@@ -3,10 +3,31 @@ import numpy as np
 import scipy.stats as sts
 import matplotlib.pyplot as plt
 import math
-
 from IPython.display import display, Markdown
-from scipy.stats import pareto
 
+class SampleMean():
+  # Инициируем наш класс с функцией распределения и размером выборки
+  def __init__(self, rv, n):
+    self.rv = rv
+    self.n = n
+
+  # Возвращает массив длинной size состоящий из выборочных средних
+  def hist_vars(self, size):
+    return [self.__mean(self.rv.rvs(self.n)) for _ in range(size)]
+
+  # Возвращает мат.ожидание для апроксимирующего нормального распределения (согласно ЦПТ)
+  def norm_mean(self):
+    return self.rv.mean()
+
+  # Возвращает стандартное отклонение для апроксимирующего нормального распределения (согласно ЦПТ)
+  def norm_std(self):
+    return math.sqrt(self.rv.var() / self.n)
+
+  # Выборочное среднее. Сумма всех элементов поделенное на колво
+  def __mean(self, vars):
+    return np.sum(vars) / np.size(vars)
+
+# Функция отрисовки результатов
 def plot(rv, hist_vars, rv_label, hist_label):
   _, ax = plt.subplots(1, 1)
   # Генерирует числа для графика
@@ -15,6 +36,11 @@ def plot(rv, hist_vars, rv_label, hist_label):
   ax.plot(x, rv.pdf(x), 'r-', lw=3, alpha=0.6, label=rv_label)
   # Рисуем гистограмму по hist_vars
   ax.hist(hist_vars, normed=True, histtype='stepfilled', alpha=0.2, label=hist_label)
+  ax.legend(loc='best', frameon=False)
+
+################
+# Основной код #
+################
 
 display(Markdown('# Распределение Парето'))
 b = 128
@@ -23,28 +49,13 @@ display(Markdown(f"## Коэффицент распеределения b={b}"))
 
 plot(pareto_rv, pareto_rv.rvs(1000), 'Pareto pdf', 'Histogram')
 
-def build_vars(n, size):
-  return [calculate_E(pareto_rv.rvs(n)) for _ in range(size)]
-
-def calculate_E(vars):
-  return np.sum(vars) / np.size(vars)
-
-def mean():
-  return pareto_rv.mean()
-
-def std(n):
-  return math.sqrt(pareto_rv.var() / n)
-
-def plot_gist(n, size, plt):
-  _, ax = plt.subplots(1, 1)
-  norm_rv = sts.norm(mean(), std(n))
-  x = np.linspace(norm_rv.ppf(0.01),
-                  norm_rv.ppf(0.99), 10)
-  ax.plot(x, norm_rv.pdf(x), 'r-', lw=3, alpha=0.6, label='Norm pdf')
-  ax.hist(build_vars(n, size), normed=True, histtype='stepfilled', alpha=0.2, label=f"n={n}")
-
-  ax.legend(loc='best', frameon=False)
-
 for n in [5, 10, 50]:
-  plot_gist(n, 1000, plt)
+  cls = SampleMean(pareto_rv, n)
+  plot(
+    sts.norm(cls.norm_mean(), cls.norm_std()),
+    cls.hist_vars(1000),
+    "Norm pdf",
+    f"n={n}"
+  )
+
 plt.show()
